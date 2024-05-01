c911ddac8c3e8170c811246a79cc9e553733ec11
Dev Goswami <devkhaab430@gmail.com>
2024-05-01 10:21:46 +0530

    initial stage 3

--- script1.js
+++ script1.js
@@ -6,6 +6,7 @@ function locomotiveAnimation() {
     const locoScroll = new LocomotiveScroll({
       el: document.querySelector("#main"),
       smooth: true,
+      smoothMobile: true
     });
     locoScroll.on("scroll", ScrollTrigger.update);
   
